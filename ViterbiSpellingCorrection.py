302df5fe2fb1de1ec565024b0be0c5ac876d2046
A.Furkan Tanriverdi <ffurkan.tanriverdi@gmail.com>
2016-12-20 23:56:01 +0300

    sth wrong

--- ViterbiSpellingCorrection.py
+++ ViterbiSpellingCorrection.py
@@ -28,11 +28,9 @@ probabilityOfWordStartsWithLetter = []  # FIRST STATE VECTOR
 
 # A[x][y] : (Transtion Probability Matrix) Durum Geçiş Olasılık Matrisi: Eğitim setinde bir kelimede x harfinden sonra y harfi gelme olasılığı
 countOfTransitionProbabilityMatrix = np.zeros(shape=(26, 27))
-transitionProbabilityMatrix = np.zeros(shape=(26, 26))
 
 # B[x][o] : (Emission Probability Matrix) Çıkış Olasılık Matrisi: Eğitim setinde x harfi olması gerekirken o harfinin görülme olasılığı.
 countOfEmissionProbabilityMatrix = np.zeros(shape=(26, 27))
-emissionProbabilityMatrix = np.zeros(shape=(26, 26))
 
 
 ################################################################### FUNCTIONS DEFINITIONS #############################################################################
@@ -43,12 +41,13 @@ def createFirstStateLetterPossibilitiesVector( ):
 	for letterIndex in range(0, 26):
 		# her harf 1 ile ilklendiği için 1 çıkarıyoruz
 		numberOfLetterInTrainSet = letterCounts[alphabet[letterIndex]] - 1
-		probabilityOfWordStartsWithLetter.append((100 * numberOfLetterInTrainSet) / (numberOfWords))
+		probabilityOfWordStartsWithLetter.append(numberOfLetterInTrainSet / numberOfWords)
 	return
 
 
 # emission ve transition matrix için olasılık matrisi
-def createProbabilityMatrixOfCountMatrix( countMatrix, probMatrix ):
+def createProbabilityMatrixOfCountMatrix( countMatrix ):
+	probabilityMatrix = np.zeros(shape=(26, 26))
 	for row in range(0, 26):
 		total = countMatrix[row][26]
 		if (total == 0):
@@ -56,28 +55,57 @@ def createProbabilityMatrixOfCountMatrix( countMatrix, probMatrix ):
 		else:
 			for column in range(0, 26):
 				count = countMatrix[row][column]
-				probability = (100 * count) / total
-				probMatrix[row][column] = probability
-	return probMatrix
+				probability = count / total
+				probabilityMatrix[row][column] = probability
+	return probabilityMatrix
 
 
-# emission ve transition matrixlerin adetlerini tutuyor
-# transition matrix için x: previous, y: current letter temsil eder
-# emission matrix için x: olması gereken harf, y ise gözlenen yanlış harfi temsil eder
-def incrementCountOfMatrixByTarget( x, y, targetCountMatrix ):
-	xIndex = alphabetEnum[x]
-	yIndex = alphabetEnum[y]
-	targetCountMatrix[xIndex, yIndex] += 1
-	targetCountMatrix[xIndex, 26] += 1
-	return targetCountMatrix
+# hangi harften sonra hangi harf deliyor sayısını tutmak için
+def incrementCountOfTransitionProbabilityMatrix( previousLetter, currentLetter ):
+	previousLetterIndex = alphabetEnum[previousLetter]
+	currentLetterIndex = alphabetEnum[currentLetter]
+	countOfTransitionProbabilityMatrix[previousLetterIndex, currentLetterIndex] += 1
+	countOfTransitionProbabilityMatrix[previousLetterIndex, 26] += 1  # totalı arttır
+	return
+
+
+def incrementCountOfEmissionProbabilityMatrix( mustBe, observed ):
+	mustBeLetterIndex = alphabetEnum[mustBe]
+	observerdLetterIndex = alphabetEnum[observed]
+	countOfEmissionProbabilityMatrix[mustBeLetterIndex, observerdLetterIndex] += 1
+	countOfEmissionProbabilityMatrix[mustBeLetterIndex, 26] += 1
+	return
 
 
 # viterbi algorithm
 def runViterbi( testWord, probabilityOfWordStartsWithLetter, transitionProbabilityMatrix, emissionProbabilityMatrix):
+	sigmaMatrix = np.zeros([testWord.__len__(), alphabet.__len__()])
 
+	firstCharIndex = alphabetEnum[testWord[0]]
+	# first iteration for the first char of observed word
+	for charIndex in range(0, alphabet.__len__()):
+		sigmaMatrix[0, charIndex] = probabilityOfWordStartsWithLetter[charIndex] * \
+		                            emissionProbabilityMatrix[firstCharIndex][charIndex]
 
+	# second iteration fot the rest of the word
+	for restOfWordCharIndex in range(1, testWord.__len__()):  # testWord geri kalan tüm harfleri değerlendiriyoruz
+		for x in range(0, alphabet.__len__()):  # tüm hidden stateler için ...
+			maxProb = 0
+			for y in range(0, alphabet.__len__()):  # ... diğer hidden stateler değerlendiriliyor
+				#      önceki durumda x olma olasılığı       * önceki gün x iken sonraki gün y olma olasılığı
+				tmp = sigmaMatrix[restOfWordCharIndex - 1][y] * transitionProbabilityMatrix[y][x]
+				if (tmp > maxProb):
+					maxProb = tmp
 
-	return
+			sigmaMatrix[restOfWordCharIndex][x] = maxProb * emissionProbabilityMatrix[
+				alphabetEnum[testWord[restOfWordCharIndex]]][x]
+
+	predictedWord = ""
+	for i in range(0, testWord.__len__()):
+		predictedChar = alphabet[np.argmax(sigmaMatrix[i])]
+		predictedWord += predictedChar
+
+	return predictedWord
 
 ######################################################################## BEGINNING OF PROGRAM ###################################################################
 
@@ -85,20 +113,31 @@ def runViterbi( testWord, probabilityOfWordStartsWithLetter, transitionProbabili
 f = open('docs.data')
 reader = csv.reader(f, delimiter=delimiterConstant)
 
-# create test and train matrix
-testMatrix = np.empty(shape=(numberOfTestCharacter, 2), dtype=np.str)
-numberOfTrainCharachter = 0
-
 # dosyadaki egitimdeki harflerin sayıları ve transition matrix sayıları hesaplanır
 testCharacterCounter = 0
+totalCharacterCounter = 0
+
+# calculate the size of test matrix
+for row in reader:
+	if (testCharacterCounter < numberOfTestCharacter):
+		totalCharacterCounter += 1
+		if row[0] != "_":
+			testCharacterCounter +=1
+
+# create test and train matrix
+testMatrix = np.empty(shape=(totalCharacterCounter, 2), dtype=np.str)
+numberOfTrainCharacters = 0
 previousLetter = ""
+counter = 0
+
+f = open('docs.data')
+reader = csv.reader(f, delimiter=delimiterConstant)
 
 for row in reader:
 	# test section
-	if (testCharacterCounter < numberOfTestCharacter):
-		testMatrix[testCharacterCounter] = row
-		if (row[0] != "_"):
-			testCharacterCounter +=1
+	if (counter < totalCharacterCounter):
+		testMatrix[counter] = row
+		counter += 1
 
 	# train section
 	else:
@@ -110,37 +149,32 @@ for row in reader:
 			previousLetter = leftSideLetter
 			continue
 
-		if (leftSideLetter == "_"):
-			previousLetter = leftSideLetter
-		else:
+		if (leftSideLetter != "_"):
 			# dosyanın ilk karakteri ya da önceki karakter underscore ise yeni kelime sayısını 1 arttır
 			if (previousLetter == "_"):
 				numberOfWords += 1
 				letterCounts[leftSideLetter] += 1
+				previousLetter = leftSideLetter
 
 			# ilk karakter ve önceki karakter underscore ise matrix update edilmez
-			if (previousLetter != "_"):
-				countOfTransitionProbabilityMatrix = incrementCountOfMatrixByTarget(previousLetter, leftSideLetter,
-				                                                                    countOfTransitionProbabilityMatrix)
+			else:
+				incrementCountOfTransitionProbabilityMatrix(previousLetter, leftSideLetter)
 				# eğer soldaki ile sağdaki aynı karakter değilse emission matrixi update et
 				if (leftSideLetter != rightSideLetter):
-					incrementCountOfMatrixByTarget(leftSideLetter, rightSideLetter, countOfEmissionProbabilityMatrix)
+					incrementCountOfEmissionProbabilityMatrix(leftSideLetter, rightSideLetter)
 
-			previousLetter = leftSideLetter
-
+				previousLetter = leftSideLetter
+		else:
+			previousLetter = "_"
 
 # ilk durum harf olasılıklarının çıkartılması
 createFirstStateLetterPossibilitiesVector()
 
 # transition matrix olasılıklarının çıkarılması
-# createStateTransitionMatrix()
-transitionProbabilityMatrix = createProbabilityMatrixOfCountMatrix(countOfTransitionProbabilityMatrix,
-                                                                   transitionProbabilityMatrix)
+transitionProbabilityMatrix = createProbabilityMatrixOfCountMatrix(countOfTransitionProbabilityMatrix)
 
 # emission matrix olasılıklarının çıkarılması
-# createEmissionProbabilityMatrix()
-emissionProbabilityMatrix = createProbabilityMatrixOfCountMatrix(countOfEmissionProbabilityMatrix,
-                                                                 emissionProbabilityMatrix)
+emissionProbabilityMatrix = createProbabilityMatrixOfCountMatrix(countOfEmissionProbabilityMatrix)
 
 testWord = ""
 trueWord = ""
@@ -150,8 +184,23 @@ numberOfFalseCorrection = 0
 for row in testMatrix:
 	# use the right side of doc
 	if (row[1] == "_"):
+		# print("------------------------------")
+		#print("Test:         " + testWord)
 		expectedWord = runViterbi(testWord, probabilityOfWordStartsWithLetter, transitionProbabilityMatrix,
 		                          emissionProbabilityMatrix)
+		# print("Predicted:    " + expectedWord)
+		# print("Right one is: " + trueWord)
+
+		if (testWord == expectedWord):
+			numberOfTrueCorrection += 1
+		else:
+			numberOfFalseCorrection += 1
+
 		testWord = ""
+		trueWord = ""
 	else:
 		testWord += row[1]
+		trueWord += row[0]
+
+accuracy = (100 * numberOfTrueCorrection) / (numberOfFalseCorrection + numberOfTrueCorrection)
+print("Accuracy: " + accuracy.__str__())
